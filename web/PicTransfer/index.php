1b90e3bd2306208dc42c16ce3856a94a8be334d0
Wayne Ngan <wayne.ngan@gmail.com>
2018-01-27 06:00:02 -0500

    back up on 01-27-18

--- web/PicTransfer/index.php
+++ web/PicTransfer/index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <title>PicTransfer</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <script type="text/javascript">
 function displayFileName()
@@ -33,7 +34,8 @@ body {
 #chosenfilelist {
     border: 1px solid #BBBBBB;
     padding: 10px 10px;
-    max-width: 500px;
+    /*max-width: 500px;*/
+    width: 500px;
     min-height: 100px;
     margin-bottom: 20px;
 }
@@ -81,6 +83,27 @@ input[type=submit]:hover {
 .displayfile:hover {
     background-color: #CCC;
 }
+
+.displayfile img {
+    padding-top: 5px;
+}
+
+.filesize {
+    color: #888;
+}
+
+@media only screen and (max-width: 700px) {
+#chosenfilelist {
+    width: 95%;
+}
+.displayfile {
+    width: 95%;
+}
+.displayfile img {
+    width: 100%;
+}
+
+}
 </style>
 
 </head>
@@ -97,25 +120,47 @@ input[type=submit]:hover {
 </form>
 
 <?php
+function isImageFile($filename)
+{
+    $extension = end(explode(".", $filename));
+    return (!strcasecmp($extension, "jpg") ||
+            !strcasecmp($extension, "png") ||
+            !strcasecmp($extension, "gif")); 
+}
+
 $dir = $_SERVER['DOCUMENT_ROOT'].'/PicTransfer/files';
-$files = array_diff(scandir($dir), array('..', '.'));
+$files_unordered = array_diff(scandir($dir), array('..', '.'));
+$files = array();
+
+foreach ($files_unordered as $thisfile)
+{
+    if (isImageFile($thisfile))
+        array_push($files, $thisfile);
+    else
+        array_unshift($files, $thisfile);
+}
 
 foreach ($files as $thisfile)
 {
     $filename = "files/".$thisfile;
+    $filesize_kB = round(filesize($dir."/".$thisfile) / 1000);
+    $filesize_MB = $filesize_kB / 1000;
 
-    print("<div class='displayfile'>\n");
+    print("<div class='displayfile'>");
+    print("<span class='filesize'>");
+    if ($filesize_MB > 1)
+        print("(".$filesize_MB." M) ");
+    else
+        print("(".$filesize_kB." k) ");
+    print("</span>");
     print("<a href='".$filename."'>".$thisfile);
 
-    $extension = end(explode(".", $thisfile));
-    if (!strcasecmp($extension, "jpg") ||
-        !strcasecmp($extension, "png") ||
-        !strcasecmp($extension, "gif")) 
+    if (isImageFile($thisfile))
     {
         print("<br/><img src='".$filename."' width='400' />");
     }
 
-    print("</a></div>");
+    print("</a></div>\n\n");
 }
 
 ?>
